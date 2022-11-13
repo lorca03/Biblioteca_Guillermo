8a79eac7b9487d4f1db5a4b70466c5d310be57fa
lorca03 <guillermolorcamartinez@gmail.com>
2022-11-14 00:31:03 +0100

    colaborador

--- colaboradores.php
+++ colaboradores.php
@@ -1,5 +1,15 @@
 <?php
 require_once 'utils/activa.php';
 require_once 'entity/colaborador.php';
-$colaborador=new Colaborador($_POST['nameCol'],$_POST['nameCol'],$_POST['iamgen']);
+require_once 'assets/file/file.php';
+$enviar = !empty($_POST['enviar']) ? $_POST['enviar'] : null;
+if ($enviar === 'Enviar') {
+    $nombre = !empty($_POST['nameCol']) ? $_POST['nameCol'] : null;
+    $descripcion = !empty($_POST['descripcionCol']) ? $_POST['descripcionCol'] : null;
+    $imagen = !empty($_POST['imagen']) ? $_POST['imagen'] : null;
+    $file=new File('imagen');
+    $colaborador = new Colaborador($nombre, $descripcion, $imagen);
+    $file->saveUploadFile($colaborador->getUrlImagen());
+}
+
 require_once 'views/colaboradores.view.php';
