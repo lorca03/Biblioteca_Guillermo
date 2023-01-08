0aa4b34fe084344d9b937419e570116e7c783da3
lorca03 <guillermolorcamartinez@gmail.com>
2023-01-09 00:42:15 +0100

    api

--- app/controllers/contacto.php
+++ app/controllers/contacto.php
@@ -7,11 +7,13 @@ use biblioteca\core\App;
 $enviar = !empty($_POST['enviar']) ? $_POST['enviar'] : null;
 $error = false;
 $erroremail = false;
+$errorlocalidad = false;
 if ($enviar === 'Enviar') {
     $nombre = !empty($_POST['name']) ? $_POST['name'] : '';
     $email = !empty($_POST['email']) ? $_POST['email'] : '';
+    $localidad = !empty($_POST['localidad']) ? $_POST['localidad'] : '';
     $mensaje = !empty($_POST['mensaje']) ? $_POST['mensaje'] : '';
-    if ($nombre == '' || $email == '' || $mensaje == '') {
+    if ($nombre == '' || $email == '' || $mensaje == ''|| $localidad == '') {
         $error=true;
         echo '<script language="javascript"> alert("Todos los campos deben estar rellenados");</script>';
     } else {
@@ -19,9 +21,25 @@ if ($enviar === 'Enviar') {
             $error=true;
             $erroremail=true;
             echo '<script language="javascript"> alert("El email no es correcto");</script>';
+        }else{
+            $cURLConnection = curl_init();
+            curl_setopt($cURLConnection, CURLOPT_URL, "https://api.mymappi.com/v2/places/autocomplete?apikey=ec8a3deb-4786-4b13-b250-a27ccd4f67d5&q=".$localidad."&auto_focus=true&layers=locality&country_code=ES&lang=es");
+            curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+            $result = curl_exec($cURLConnection);
+            curl_close($cURLConnection);
+            $json_result = json_decode($result,true);
+            if (count($json_result['data'])==0){
+                $error=true;
+                $errorlocalidad=true;
+                echo '<script language="javascript"> alert("La Localidad no es correcta");</script>';
+            }else{
+                $localidad=$json_result['data'][0]['locality'];
+                $errorlocalidad=false;
+            }
+            //var_dump( $json_result['data']/*[0]['locality']*/);
         }
     }
-    if ($error==false && $erroremail==false) {
+    if ($error==false && $erroremail==false && $errorlocalidad==false) {
         $repoMensaje = App::getRepository(MensajeRepositorio::class);
         $repoMensaje->save(new Mensaje($email,$nombre,$mensaje));
         $mailer=App::get('mailer');
--- app/views/contacto.view.php
+++ app/views/contacto.view.php
@@ -5,92 +5,105 @@
 require_once 'partials/head.php';
 ?>
 <style>
-.imagenesFooter {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
+    .imagenesFooter {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 
-.imagenesFooter img {
-    width: 150px;
-    height: 90px;
-}
-#contactaContai{
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding-bottom: 30px;
-}
-#contacta{
-  width: 350px;
-  height: 270px;
-}
+    .imagenesFooter img {
+        width: 150px;
+        height: 90px;
+    }
+
+    #contactaContai {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding-bottom: 30px;
+    }
+
+    #contacta {
+        width: 350px;
+        height: 270px;
+    }
 </style>
 <body>
 
 
-  <!-- ***** Header Area Start ***** -->
-  <?php require_once 'partials/header.part.php'   ?>
-  <!-- ***** Header Area End ***** -->
+<!-- ***** Header Area Start ***** -->
+<?php require_once 'partials/header.part.php' ?>
+<!-- ***** Header Area End ***** -->
 
-  <div class="container">
+<div class="container">
     <div class="row">
-      <div class="col-lg-12">
-        <div class="page-content">
-          <h1 id="titulo">Contacta con nosotros</h1>
-          <div class="row">
-            <div class="col-6">
-              <form action="" method="post">
+        <div class="col-lg-12">
+            <div class="page-content">
+                <h1 id="titulo">Contacta con nosotros</h1>
                 <div class="row">
-                  <label>Nombre</label>
-                  <input type="text" name="name" value="<?php if (isset($nombre) && $error==true) {
-                                                          echo $nombre;
-                                                        } else {
-                                                          echo '';
-                                                        } ?>">
+                    <div class="col-6">
+                        <form action="" method="post">
+                            <div class="row">
+                                <label>Nombre</label>
+                                <input type="text" name="name" value="<?php if (isset($nombre) && $error == true) {
+                                    echo $nombre;
+                                } else {
+                                    echo '';
+                                } ?>">
+                            </div>
+                            <div class="row">
+                                <label>E-mail</label>
+                                <input type="email" name="email"
+                                       value="<?php if (isset($email) && $error == true && $erroremail == false) {
+                                           echo $email;
+                                       } else {
+                                           echo '';
+                                       } ?>">
+                            </div>
+                            <div class="row">
+                                <label>Localidad</label>
+                                <input type="text" name="localidad"
+                                       value="<?php if (isset($localidad) && $error == true && $errorlocalidad == false) {
+                                    echo $localidad;
+                                } else {
+                                    echo '';
+                                } ?>">
+                            </div>
+                            <div class="row">
+                                <label>Mensaje</label>
+                                <textarea name="mensaje" cols="30" rows="5"
+                                          name="mensaje"><?php if (isset($mensaje) && $error == true) {
+                                        echo $mensaje;
+                                    } else {
+                                        echo '';
+                                    } ?></textarea>
+                            </div>
+                            <br>
+                            <input type="submit" name="enviar" value="Enviar">
+                        </form>
+                    </div>
+                    <div id="contactaContai" class="col-6">
+                        <img id="contacta" src="assets/images/contacta.PNG" alt="NO Funciona la imagen">
+                    </div>
                 </div>
-                <div class="row">
-                  <label>E-mail</label>
-                  <input type="email" name="email" value="<?php if (isset($email) && $error==true && $erroremail==false) {
-                                                            echo $email;
-                                                          }else {
-                                                            echo '';
-                                                          }?>">
-                </div>
-                <div class="row">
-                  <label>Mensaje</label>
-                  <textarea name="mensaje" cols="30" rows="5" name="mensaje"><?php if (isset($mensaje) && $error==true) {
-                                                                                      echo $mensaje;
-                                                                                    }else {
-                                                                                      echo '';
-                                                                                    } ?></textarea>
-                </div>
-                <br>
-                <input type="submit" name="enviar" value="Enviar">
-              </form>
             </div>
-            <div id="contactaContai" class="col-6">
-              <img id="contacta" src="assets/images/contacta.PNG" alt="NO Funciona la imagen">
-            </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
+</div>
 
-  <?php require_once 'partials/footer.part.php'   ?>
+<?php require_once 'partials/footer.part.php' ?>
 
 
-  <!-- Scripts -->
-  <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!-- Scripts -->
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
 
-  <script src="assets/js/isotope.min.js"></script>
-  <script src="assets/js/owl-carousel.js"></script>
-  <script src="assets/js/tabs.js"></script>
-  <script src="assets/js/popup.js"></script>
-  <script src="assets/js/custom.js"></script>
+<script src="assets/js/isotope.min.js"></script>
+<script src="assets/js/owl-carousel.js"></script>
+<script src="assets/js/tabs.js"></script>
+<script src="assets/js/popup.js"></script>
+<script src="assets/js/custom.js"></script>
 
 
 </body>
